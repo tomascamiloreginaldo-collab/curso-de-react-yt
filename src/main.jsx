bdbb190e2e10a6f4f8c0ef49e01445c8a35fb81a
Lotyme <radjathomas14@gmail.com>
2026-05-02 08:20:06 +0200

    fix: corrigi nome do TaskPage

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import TaskPage from "./pagens/Taskpage.jsx";
+import TaskPage from "./pagens/TaskPage.jsx";
 
 const router = createBrowserRouter([
   {
