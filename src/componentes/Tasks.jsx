682988ffb51ed8f719634e1a58309e35aae63761
Lotyme <radjathomas14@gmail.com>
2026-05-02 08:30:56 +0200

    fix: corrigi button children

--- src/componentes/Tasks.jsx
+++ src/componentes/Tasks.jsx
@@ -1,6 +1,6 @@
 import { ChevronRightIcon, Trash2Icon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import Button from "./button";
+import Button from "./Button";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   const navigate = useNavigate();
